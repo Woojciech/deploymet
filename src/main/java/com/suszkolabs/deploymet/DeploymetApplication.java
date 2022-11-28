c4119bfeaf2d6ed8e426f5d537210d1ac729bbf4
Woojciech <wojtek@suszko.pl>
2022-11-28 11:36:22 +0100

    Initial commit

---
+++ src/main/java/com/suszkolabs/deploymet/DeploymetApplication.java
@@ -0,0 +1,13 @@
+package com.suszkolabs.deploymet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeploymetApplication.class, args);
+    }
+
+}
